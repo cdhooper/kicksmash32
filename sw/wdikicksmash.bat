01b6f6da2862ce7d2af99e36824d46177533122e
Adam Polkosnik [fun] <apolkosnik@gmail.com>
2025-04-10 19:23:16 -0400

    Update wdikicksmash.bat
    capital O

--- sw/wdikicksmash.bat
+++ sw/wdikicksmash.bat
@@ -1,5 +1,5 @@
 @echo "Getting wdi-simple from"
 @echo "https://github.com/rogerclarkmelbourne/Arduino_STM32/blob/master/drivers/win/wdi-simple.exe?raw=true"
-wget https://github.com/rogerclarkmelbourne/Arduino_STM32/blob/master/drivers/win/wdi-simple.exe?raw=true" -o wdi-simple.exe
+wget https://github.com/rogerclarkmelbourne/Arduino_STM32/blob/master/drivers/win/wdi-simple.exe?raw=true" -O wdi-simple.exe
 @echo "Installing wdi-simple as a driver for KickSmash"
 wdi-simple.exe --vid 0x1209 --pid 0x1610 --type 3 --name "KickSmash Prg" --dest "maple-serial"
